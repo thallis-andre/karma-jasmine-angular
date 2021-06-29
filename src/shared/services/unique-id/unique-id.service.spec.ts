cefd217e22a2e08bba5a8446407d094aa6ba8d29
thallis-andre <thallis-andre@hotmail.com>
2021-06-28 22:22:54 -0300

    Unique id service

---
+++ src/shared/services/unique-id/unique-id.service.spec.ts
@@ -0,0 +1,36 @@
+import { UniqueIdService } from './unique-id.service';
+
+describe(UniqueIdService.name, () => {
+  let service: UniqueIdService = null;
+  beforeEach(() => {
+    service = new UniqueIdService();
+  });
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should generate id when called with prefix`, () => {
+    const id = service.generateUniqueIdWithPrefix('app');
+    expect(id.startsWith('app-')).toBeTrue();
+  });
+
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should not generate dulicated ids`, () => {
+    const ids = new Set();
+    for (let i = 0; i < 50; i++) {
+      ids.add(service.generateUniqueIdWithPrefix('app'));
+    }
+    expect(ids.size).toBe(50);
+  });
+
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should return the number when called`, () => {
+    service.generateUniqueIdWithPrefix('app');
+    service.generateUniqueIdWithPrefix('app');
+
+    expect(service.getNumberOfGeneratedIds()).toBe(2);
+  });
+
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should throw when called with empty`, () => {
+    const emptyValues = [null, undefined, '', '1'];
+    emptyValues.forEach((emptyValue) => {
+      expect(() => service.generateUniqueIdWithPrefix(emptyValue))
+        .withContext(`Empty value: ${emptyValue}`)
+        .toThrow();
+    });
+  });
+});
