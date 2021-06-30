e6ba6811e109b015abe92269e9bbf4ed8e0900fb
thallis-andre <thallis-andre@hotmail.com>
2021-06-30 19:36:01 -0300

    Like widget component

---
+++ src/shared/services/components/like-widget/like-widget.component.ts
@@ -0,0 +1,30 @@
+import { UniqueIdService } from './../../unique-id/unique-id.service';
+import { EventEmitter, Input, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { faThumbsUp } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-like-widget',
+  templateUrl: './like-widget.component.html',
+  styleUrls: ['./like-widget.component.scss'],
+})
+export class LikeWidgetComponent implements OnInit {
+  @Output() public liked = new EventEmitter<void>();
+  @Input() public likes = 0;
+  @Input() public id: string = null;
+
+  public fonts = { faThumbsUp };
+
+  constructor(private uniqueIdServeice: UniqueIdService) {}
+
+  ngOnInit(): void {
+    if (!this.id) {
+      this.id =
+        this.uniqueIdServeice.generateUniqueIdWithPrefix('liked-widget');
+    }
+  }
+
+  public like(): void {
+    this.liked.emit();
+  }
+}
